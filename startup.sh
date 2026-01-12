6f2e81a5472e2c8906af8392643976a4df58e127
jazwa <jazwang@proton.me>
2026-01-12 10:45:22 -0500

    conditional tap aciton

--- startup.sh
+++ startup.sh
@@ -1,2 +1,5 @@
 xset r rate 350 50
-xinput set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Tap Action" 0, 0, 0, 0, 0, 0, 0
+
+if xinput list --name-only | grep -Fxq "SynPS/2 Synaptics TouchPad"; then
+  xinput set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Tap Action" 0, 0, 0, 0, 0, 0, 0
+fi
